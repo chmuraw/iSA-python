14a31be7d0578cafa20b97c988eb20675935ecfb
chmuraw <katarzzur@gmail.com>
2019-11-04 15:31:49 +0100

    Zadanie1

--- homeworks/HW03_improvement.py
+++ homeworks/HW03_improvement.py
@@ -1,12 +1,25 @@
-## Zadanie 1 : konwersja z Cel na Fah
 
-def konwersja(skala = None, temperatura = None):
-    if skala == "C":
-        return "F", (temperatura * 9/5) + 32.0
-    elif skala == "F":
-        return "C", (temperatura - 32) * 5/9
-    else:
-        print("Skala musi byc F lub C!")
+# def konwersja_temperatury():
+# # # Napisz program do przeliczania stopni Celsjusza na Fahrenheita i odwrotnie (wyświetlając wzór i kolejne obliczenia)
+# def konwersja(skala = None, temperatura = None):
+#     if skala == "C" or skala == "c":
+#         return "F", (temperatura * 9/5) + 32.0
+#         print("F = (C * 9/5) + 32")
+#     elif skala == "F" or skala == "f":
+#         return "C", (temperatura - 32) * 5/9
+#     else:
+#         print("Skala musi byc F lub C!")
+#
+# skala = input("Proszę wybrac skalę (F) lub (C): ")
+# temperatura = float(input("Proszę podac temperaturę: "))
+#
+# # Przypisanie zmiennych wprowadzonych przez użytkownika
+# a, b = konwersja(skala, temperatura)
+# # Wyświetlenie odpowiedniego wzoru
+# if skala == "C" or skala == "c":
+#     print("F = (C * 9/5) + 32")
+# else:
+#     print("C = (F - 32) * 5/9")
+# # Podanie ostatecznego wyniku konwersji
+# print(temperatura, "stopni", skala, "to", round(b, 2), "stopni", a)
 
-skala = input("Proszę wybra skalę (F) lub (C): ")
-temperatura = float(input("Proszę podac temperaturę: "))
