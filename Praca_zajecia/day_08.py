0d9fe5af555e807fd15727f43f0eb9aa238fe8cb
chmuraw <katarzzur@gmail.com>
2019-11-14 20:37:38 +0100

    przyklady z zajec 08 - zadanie do dokonczenia!!!

--- Praca_zajecia/day_08.py
+++ Praca_zajecia/day_08.py
@@ -101,3 +101,37 @@ import cStringIO
 imgdata = urlopen('https://wallpaperlist.com/').read()
 img = Image.open(cStringIO.StringIO(imgdata))
 img.save("pic1" + ".png")
+
+
+
+## rozwiazanie z stackoverload
+import re
+import requests
+from bs4 import BeautifulSoup
+
+site = 'http://pixabay.com'
+
+response = requests.get(site)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+img_tags = soup.find_all('img')
+
+urls = [img['src'] for img in img_tags]
+
+
+for url in urls:
+    filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+    with open(filename.group(1), 'wb') as f:
+        if 'http' not in url:
+            # sometimes an image source can be relative
+            # if it is provide the base url which also happens
+            # to be the site variable atm.
+            url = '{}{}'.format(site, url)
+        response = requests.get(url)
+        f.write(response.content)
+
+        ## do przerobienia!!!
+        https: // stackoverflow.com / questions / 18408307 / how - to - extract - and -download - all - images -
+        from
+
+        -a - website - using - beautifulsoup
